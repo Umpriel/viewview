97bab97b4e3b4daa1d9fe73a7e505a8d18e5a27c
Thomas Buckley-Houston <tom@tombh.co.uk>
2026-01-13 19:02:43 -0300

    Compile .pmtile from world.vrt
    This skips out the whole merged.tif step, so saves a lot of time.

--- scripts/make_pmtiles.bash
+++ scripts/make_pmtiles.bash
@@ -18,32 +18,11 @@ function make_pmtiles {
 	rm "$output" || true
 	archive="$ARCHIVE_DIR"
 	world_vrt="$archive"/world.vrt
-	merged="$OUTPUT_DIR"/merged.tif
 
 	echo "Using archive at $archive"
 
 	# Collate all the heatmap GeoTiffs into a single virtual file.
 	gdalbuildvrt "$world_vrt" "$archive"/*.tiff
-
-	# Merge all the heatmap GeoTiffs into one big GeoTiff.
-	#
-	# Note: Even though this is just a temporary file, compression is still useful to help reduce
-	# disk IO. Bear in mind that `merged.tif` is constantly queried by the final pmtiler, so any
-	# reduction in disk IO can speed up the total runtime.
-	#
-	# TODO: Most of the time in this command is spent single threaded, which means up to 5 hours
-	# for the whole world. Perhaps produce multiple `merged-part.tiff` as pre-step, then merge
-	# for the whole world. Perhaps produce multiple `merged-part.tiff` as pre-step, then merge
-	# those for the final step?
-	gdal_translate \
-		-co TILED=YES \
-		-co BIGTIFF=YES \
-		-co BLOCKXSIZE=512 \
-		-co BLOCKYSIZE=512 \
-		-co COMPRESS=ZSTD \
-		--config GDAL_NUM_THREADS ALL_CPUS \
-		--config NUM_THREADS ALL_CPUS \
-		"$world_vrt" "$merged"
 
 	# Create overviews to speed up tile creation at lower zoom levels.
 	gdaladdo \
@@ -51,11 +30,11 @@ function make_pmtiles {
 		--config BIGTIFF YES \
 		--config COMPRESS_OVERVIEW DEFLATE \
 		--config GDAL_NUM_THREADS ALL_CPUS \
-		"$merged" \
+		"$world_vrt" \
 		2 4 8 16 32 64 128 256
 
 	# Create the global `.pmtile`
-	uv run scripts/to_pmtiles.py "$merged" "$output" \
+	uv run scripts/to_pmtiles.py "$world_vrt" "$output" \
 		--min_zoom 0 \
 		--max_zoom 11
 
