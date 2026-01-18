00121532d113c660f9065705c916480cbf43a03e
Thomas Buckley-Houston <tom@tombh.co.uk>
2026-01-18 14:59:24 -0300

    Mapbox search box
    Works pretty well. Just can't find Mount Everest??

--- website/src/Layout.svelte
+++ website/src/Layout.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
-  import { onDestroy } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
   import 'maplibre-gl/dist/maplibre-gl.css';
   import 'accessible-nprogress/src/styles.css';
+  import { MapboxSearchBox } from '@mapbox/search-js-web';
   import Sidebar from './Sidebar.svelte';
   import { state } from './state.svelte.ts';
+
+  onMount(() => {
+    const searchBox = new MapboxSearchBox();
+    searchBox.accessToken =
+      'pk.eyJ1IjoidG9tYmgiLCJhIjoiY2p4cWlqNnY1MDFhZDNscXc5YXJpcTJzciJ9.7gGR5t8KEAY0ZoXfTVBcng';
+    searchBox.options = {
+      types: 'poi,place,country',
+      poi_category: 'mountain,natural_feature',
+    };
+    searchBox.addEventListener('retrieve', (e) => {
+      const feature = e.detail;
+      const coordinates = feature.features[0]?.geometry.coordinates;
+      state.map?.flyTo({
+        center: [coordinates[0], coordinates[1]],
+        zoom: 11,
+      });
+    });
+
+    // @ts-expect-error: `document` can't be null.
+    document.querySelector('#search-box').appendChild(searchBox);
+  });
 
   onDestroy(() => {
     state.map?.remove();
@@ -18,6 +40,8 @@
 
 <Sidebar />
 
+<div id="search-box"></div>
+
 <main>
 	<slot />
 </main>
@@ -27,5 +51,12 @@
 		position: absolute;
 		height: 100%;
 		width: 100%;
+	}
+
+	#search-box {
+		position: absolute;
+		width: 300px;
+		margin-left: 70px;
+		margin-top: 17px;
 	}
 </style>
