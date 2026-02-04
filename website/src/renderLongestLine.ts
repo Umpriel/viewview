f66d8f1f1abc2755d3b0de02da161dcc50c42403
Thomas Buckley-Houston <tom@tombh.co.uk>
2026-02-04 12:34:46 +0100

    Better contrast colour for line of sight polygon
    Fixes #38

--- website/src/renderLongestLine.ts
+++ website/src/renderLongestLine.ts
@@ -22,9 +22,9 @@ export function setupLongestLines(coordFromURL: string | undefined) {
     type: 'fill',
     source: 'longest-line',
     paint: {
-      'fill-color': '#fd6612',
-      'fill-outline-color': '#141f41',
-      'fill-opacity': 0.5,
+      'fill-color': '#5BC0EB',
+      'fill-outline-color': '#ffffff',
+      'fill-opacity': 0.8,
     },
   });
 
