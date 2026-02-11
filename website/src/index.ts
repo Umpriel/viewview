a50b8ad5f0f7e7a775a40a21d84a83d2bea9f4b2
Thomas Buckley-Houston <tom@tombh.co.uk>
2026-02-11 15:26:19 +0100

    Support importing SASS vars in `.svelte` files

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte';
 import App from './App.svelte';
-import './index.scss';
+import './styles/index.scss';
 
 const app = mount(App, {
   target: document.getElementById('root') as HTMLElement,
